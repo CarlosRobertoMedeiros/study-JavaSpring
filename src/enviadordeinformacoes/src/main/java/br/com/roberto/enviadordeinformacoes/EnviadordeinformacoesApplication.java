0f8668c31a9e69d2da0f940b49a7090c1582ae95
CarlosRobertoMedeiros <carlosmedeiroslima@gmail.com>
2019-12-09 06:00:23 -0300

    Foi implementado a Rotina do envio de Email, Porém, é necessário realizar as configurações de cada serviço em particular, o que para esse momento não é o foco

--- src/enviadordeinformacoes/src/main/java/br/com/roberto/enviadordeinformacoes/EnviadordeinformacoesApplication.java
+++ src/enviadordeinformacoes/src/main/java/br/com/roberto/enviadordeinformacoes/EnviadordeinformacoesApplication.java
@@ -3,6 +3,14 @@ package br.com.roberto.enviadordeinformacoes;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * O Básico do Sistema está pronto
+ * Basta Apenas as configurações relativas as contas do Google 
+ * Para o envio de Email
+ * Nesse momento não é necessário
+ * @since 1.8
+ * */
+
 @SpringBootApplication
 public class EnviadordeinformacoesApplication {
 
